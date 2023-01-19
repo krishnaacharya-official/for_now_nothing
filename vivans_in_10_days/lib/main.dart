1dd3bd6071604b8ac728f824ca114595f2239822
krishnaacharya-official <krishnaacharya.official@gmail.com>
2023-01-19 22:31:49 +0530

    Changes

--- vivans_in_10_days/lib/main.dart
+++ vivans_in_10_days/lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:vivans_in_10_days/cubit/internet_cubit.dart';
-import 'package:vivans_in_10_days/helpers/connectivity_dialog.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 // class MyApp extends StatelessWidget {
@@ -61,70 +58,98 @@ void main() {
 //           // Other routes
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  bool _isDialogDisplayed = false;
+  const MyApp({super.key});
+  final bool _isDialogDisplayed = false;
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return BlocProvider(
+  //       create: (context) => ConnectivityCubit(),
+  //       child: MaterialApp(
+  //           home: Scaffold(
+  //               body: StreamBuilder<ConnectivityResult>(
+  //                   stream: ConnectivityCubit().streamValue,
+  //                   builder: (context, snapshot) {
+  //                     // if (!snapshot.hasData) {
+  //                     if (snapshot.data == ConnectivityResult.none ||
+  //                         !snapshot.hasData) {
+  //                       return MediaQuery(
+  //                         data: MediaQuery.of(context)
+  //                             .copyWith(textScaleFactor: 1.0),
+  //                         child: Dialog(
+  //                           child: Column(
+  //                             children: <Widget>[
+  //                               const Text('No internet connection'),
+  //                               FilledButton(
+  //                                 onPressed: () {
+  //                                   BlocProvider.of<ConnectivityCubit>(context)
+  //                                       .checkConnectivity();
+  //                                 },
+  //                                 child:
+  //                                     const Text('Check Internet Connectivity'),
+  //                               ),
+  //                             ],
+  //                           ),
+  //                         ),
+  //                       );
+  //                     } else if (snapshot.data == ConnectivityResult.mobile ||
+  //                         snapshot.data == ConnectivityResult.ethernet ||
+  //                         snapshot.data == ConnectivityResult.wifi) {
+  //                       return Stack(
+  //                         children: <Widget>[
+  //                           // Other widgets
+  //                           BlocConsumer<ConnectivityCubit, bool>(
+  //                             listener: (context, state) {
+  //                               if (!state) {
+  //                                 _isDialogDisplayed = true;
+  //                               } else {
+  //                                 _isDialogDisplayed = false;
+  //                                 ScaffoldMessenger.of(context).showSnackBar(
+  //                                     const SnackBar(
+  //                                         content: Text(
+  //                                             'Welcome back, you are online now')));
+  //                               }
+  //                             },
+  //                             builder: (context, state) {
+  //                               return _isDialogDisplayed
+  //                                   ? const ConnectivityDialog()
+  //                                   : Container(child: const Text("Hello"));
+  //                             },
+  //                           ),
+  //                           // Your main layout
+  //                         ],
+  //                       );
+  //                       // Other routes
+  //                     }
+  //                     return const Center(child: CircularProgressIndicator());
+  //                   }))));
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        create: (context) => ConnectivityCubit(),
-        child: MaterialApp(
-            home: Scaffold(
-                body: StreamBuilder<ConnectivityResult>(
-                    stream: ConnectivityCubit().streamValue,
-                    builder: (context, snapshot) {
-                      // if (!snapshot.hasData) {
-                      if (snapshot.data == ConnectivityResult.none ||
-                          !snapshot.hasData) {
-                        return MediaQuery(
-                          data: MediaQuery.of(context)
-                              .copyWith(textScaleFactor: 1.0),
-                          child: Dialog(
-                            child: Column(
-                              children: <Widget>[
-                                const Text('No internet connection'),
-                                FilledButton(
-                                  onPressed: () {
-                                    BlocProvider.of<ConnectivityCubit>(context)
-                                        .checkConnectivity();
-                                  },
-                                  child:
-                                      const Text('Check Internet Connectivity'),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      } else if (snapshot.data == ConnectivityResult.mobile ||
-                          snapshot.data == ConnectivityResult.ethernet ||
-                          snapshot.data == ConnectivityResult.wifi) {
-                        return Stack(
-                          children: <Widget>[
-                            // Other widgets
-                            BlocConsumer<ConnectivityCubit, bool>(
-                              listener: (context, state) {
-                                if (!state) {
-                                  _isDialogDisplayed = true;
-                                } else {
-                                  _isDialogDisplayed = false;
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                          content: Text(
-                                              'Welcome back, you are online now')));
-                                }
-                              },
-                              builder: (context, state) {
-                                return _isDialogDisplayed
-                                    ? const ConnectivityDialog()
-                                    : Container(child: const Text("Hello"));
-                              },
-                            ),
-                            // Your main layout
-                          ],
-                        );
-                        // Other routes
-                      }
-                      return const Center(child: CircularProgressIndicator());
-                    }))));
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: StreamBuilder<ConnectivityResult>(
+          stream: Connectivity().onConnectivityChanged,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData || snapshot.data == ConnectivityResult.none) {
+              return const AlertDialog(
+                title: Text('No internet connection'),
+                content: Text('Please check your internet connection.'),
+              );
+            }
+            if (snapshot.hasData &&
+                (snapshot.data == ConnectivityResult.mobile ||
+                    snapshot.data == ConnectivityResult.wifi ||
+                    snapshot.data == ConnectivityResult.ethernet)) {
+              return const Text("Hello there");
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
   }
 }
