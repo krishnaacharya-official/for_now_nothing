3a3698a18af74cb36c573e7110aad3b7ff02349a
krishnaacharya-official <krishnaacharya.official@gmail.com>
2023-01-20 10:40:52 +0530

    removed alert dialog for internet connectivity

--- vivans_in_10_days/lib/todo.dart
+++ vivans_in_10_days/lib/todo.dart
@@ -1,1 +1,3 @@
 /**feature: Customize the dialog box  */
+
+/**feature:You can make the internet dialog pop instead of blocking the entire screen, You can find that in this github commit named something like removed alert dialog for internet connectivity */
